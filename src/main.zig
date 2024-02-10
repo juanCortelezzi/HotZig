86199b5a8998993bc699c36686f52192cd11beac
J.B.C <juanbicortelezzi@gmail.com>
2024-02-10 21:39:55 +1300

    better jumping?

--- src/main.zig
+++ src/main.zig
@@ -5,12 +5,13 @@ const c = @cImport({
 
 const SCREEN_WIDTH = 800;
 const SCREEN_HEIGHT = 450;
-const GRAVITY: f32 = 0.9;
+const GRAVITY: f32 = 9.81;
+const INITIAL_JUMP_VELOCITY: f32 = 50;
 
 const Player = struct {
     position: c.Vector2,
-    size: c.Vector2,
     velocity: c.Vector2,
+    size: c.Vector2,
     jumping: bool,
 };
 
@@ -24,61 +25,53 @@ pub fn main() !void {
             .x = 50,
             .y = SCREEN_HEIGHT - 100,
         },
-        .size = .{
-            .x = 20,
-            .y = 20,
-        },
         .velocity = .{
             .x = 0,
             .y = 0,
+        },
+        .size = .{
+            .x = 20,
+            .y = 20,
         },
         .jumping = false,
     };
 
     var jump_time: f32 = 0;
     var jumping_locked = false;
-    const initial_jump_velocity = 100;
 
     while (!c.WindowShouldClose()) {
         // UPDATE
         if (c.IsKeyDown(c.KEY_SPACE) and !player.jumping and !jumping_locked) {
-            player.velocity.y = initial_jump_velocity;
             player.jumping = true;
+            player.velocity.y = INITIAL_JUMP_VELOCITY;
             jumping_locked = true;
             jump_time = 0;
         }
 
         if (c.IsKeyDown(c.KEY_R)) {
             jumping_locked = false;
-            player.velocity.y = 0;
             player.position.y = SCREEN_HEIGHT - 100;
+            player.velocity.y = 0;
         }
 
         if (player.jumping) {
             jump_time += c.GetFrameTime();
+            const asdf = jump_time / 6;
 
             // y = v0 * t - 0.5 * g * t^2
             // v = v0 - g * t
 
-            const delta_y = initial_jump_velocity * jump_time - 0.5 * GRAVITY * std.math.pow(f32, jump_time, 2);
-            if (player.velocity.y < 0) {
-                player.position.y += delta_y;
-            } else {
-                player.position.y -= delta_y;
-            }
+            const delta_y = player.velocity.y * asdf - 0.5 * GRAVITY * std.math.pow(f32, asdf, 2);
+            const delta_v = GRAVITY * asdf;
 
-            var vel_is_pos = player.velocity.y > 0;
-            player.velocity.y -= (initial_jump_velocity / 10) - GRAVITY * jump_time;
-            if (vel_is_pos and !(player.velocity.y > 0)) {
-                jump_time = 0;
-            }
+            player.position.y -= delta_y;
+            player.velocity.y -= delta_v;
 
             if (player.position.y > SCREEN_HEIGHT - 100) {
                 player.position.y = SCREEN_HEIGHT - 100;
                 player.jumping = false;
             } else if (player.position.y < 0) {
                 player.position.y = 0;
-                player.jumping = false;
             }
         }
 
@@ -102,9 +95,10 @@ pub fn main() !void {
         c.DrawText(msg, 10, offset, 20, c.DARKGRAY);
         offset += 20;
 
-        msg = try std.fmt.bufPrintZ(&buffer, comptime "velocity: {}", .{@as(i32, @intFromFloat(player.velocity.y))});
+        msg = try std.fmt.bufPrintZ(&buffer, comptime "jump_time: {}", .{@as(i32, @intFromFloat(jump_time))});
         c.DrawText(msg, 10, offset, 20, c.DARKGRAY);
         offset += 20;
+
         c.DrawRectangleV(player.position, player.size, c.RED);
     }
 }
